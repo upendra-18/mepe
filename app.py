2b8dcc77136fb59ee2e5570186ab8ebdcfd9d386
upendra-18 <upendrareddy637@gmail.com>
2026-02-14 06:05:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ import streamlit as st
 import requests
 import numpy as np
 import openai
-from gradio_client import Client
-from gradio_client.utils import handle_file
+from gradio_client import Client, handle_file
 import tempfile
 
 # -----------------------
